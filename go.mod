82c3444b9af925cc08ea320fe1edb7b2b2ad6ae1
twist <bettercallssx@163.com>
2020-02-02 07:18:27 -0800

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/songshixuan/twist.git
 
 go 1.13
+
+require (
+	github.com/segmentio/kafka-go v0.3.5
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/segmentio/kafka-go"
 )
 
+//install lib(match versition) config
 func main() {
-	fmt.Println("")
+	topic := "my-topic2"
+	partition := 0
+
+	conn, e := kafka.DialLeader(context.Background(), "tcp", "localhost:32770", topic, partition)
+	if e != nil {
+		fmt.Println(e.Error())
+	}
+	//conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	fmt.Println("writes")
+	i, e := conn.Write(
+		[]byte("one!"),
+	)
+	fmt.Println(i, e)
+	conn.Close()
 }
